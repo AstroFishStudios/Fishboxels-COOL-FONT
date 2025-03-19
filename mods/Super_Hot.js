e4d0ae075b69a395b90890c9ce5958a745980687
Fischer Brown <5137455873@student.cms.k12.nc.us>
2025-03-19 10:09:45 -0400

    Create Super_Hot.js

---
+++ mods/Super_Hot.js
@@ -0,0 +1,33 @@
+// SUPER HOT Mod
+
+// Function to check availability of required dependencies
+function whenAvailable(names, callback) {
+    var interval = 10; // ms
+    window.setTimeout(function() {
+        let bool = true;
+        for(let i = 0; i < names.length; i++) {
+            if (!window[names[i]]) {
+                bool = false;
+            }
+        }
+        if (bool) {
+            callback();
+        } else {
+            whenAvailable(names, callback);
+        }
+    }, interval);
+}
+
+var modName = "mods/super_hot.js";
+
+whenAvailable(["elements"], function() {
+    elements.super_hot = {
+        color: "#ff4500",
+        behavior: behaviors.WALL,
+        category: "solids",
+        state: "solid",
+        density: 8000,
+        temp: 1e12, // 1 trillion degrees
+        conduct: true,
+    };
+});
